34e4ae63323415bc586b797e311c5289a68486ea
Gustavo Rezin Durigon <gustavo.rezin@gmail.com>
2024-08-07 15:19:15 -0300

    :test_tube: test: componente `Menu`

---
+++ src/components/menu/test.tsx
@@ -0,0 +1,52 @@
+import { renderWithTheme } from '@/utils/tests/helpers'
+import { Menu } from '.'
+import { fireEvent, screen } from '@testing-library/react'
+
+describe('<Menu />', () => {
+  it('should render the menu', () => {
+    renderWithTheme(<Menu />)
+
+    expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument()
+    expect(screen.getByLabelText(/won games/i)).toBeInTheDocument()
+    expect(screen.getByLabelText(/search/i)).toBeInTheDocument()
+    expect(screen.getByLabelText(/open shopping cart/i)).toBeInTheDocument()
+  })
+
+  it('should render the open/close mobile menu', () => {
+    renderWithTheme(<Menu />)
+
+    // selecionar o MenuFull
+    const fullMenuElement = screen.getByRole('navigation', { hidden: true })
+
+    // verificar se o menu ta escondido
+    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('true')
+    expect(fullMenuElement).toHaveStyle({ opacity: 0 })
+
+    // clicar no botao de abrir o menu e verificar se abriu
+    fireEvent.click(screen.getByLabelText(/open menu/i))
+    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('false')
+    expect(fullMenuElement).toHaveStyle({ opacity: 1 })
+
+    // clicar no botao de fechar o menu e verificar se fechou
+    fireEvent.click(screen.getByLabelText(/close menu/i))
+    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('true')
+    expect(fullMenuElement).toHaveStyle({ opacity: 0 })
+  })
+
+  it(`should show register box when logged out`, () => {
+    renderWithTheme(<Menu />)
+    expect(screen.queryByText(/my account/i)).not.toBeInTheDocument()
+    expect(screen.queryByText(/wishlist/i)).not.toBeInTheDocument()
+    expect(screen.getByText(/log in now/i)).toBeInTheDocument()
+    expect(screen.getByText(/sign up/i)).toBeInTheDocument()
+  })
+
+  it(`should show account and wishlist when logged in`, () => {
+    renderWithTheme(<Menu username="gu" />)
+
+    expect(screen.getByText(/my account/i)).toBeInTheDocument()
+    expect(screen.getByText(/wishlist/i)).toBeInTheDocument()
+    expect(screen.queryByText(/log in now/i)).not.toBeInTheDocument()
+    expect(screen.queryByText(/sign up/i)).not.toBeInTheDocument()
+  })
+})
