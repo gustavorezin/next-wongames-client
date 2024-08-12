e1c3f8903b3c41868e3f042ed8b8196e482a2be5
Gustavo Rezin Durigon <gustavo.rezin@gmail.com>
2024-08-12 20:45:13 -0300

    :test_tube: test: componente `Banner`

---
+++ src/components/banner/test.tsx
@@ -0,0 +1,30 @@
+import { screen } from '@testing-library/react'
+
+import { renderWithTheme } from '@/utils/tests/helpers'
+import { Banner } from '.'
+
+const props = {
+  img: 'https://www.arx.pt/wp-content/uploads/2016/09/Malveira-7-1042x580.jpg',
+  title: 'Defy death',
+  subtitle: '<p>Play the new <strong>CrashLands</strong> season</p>',
+  buttonLabel: 'Buy now',
+  buttonLink: '/games/defy-death'
+}
+
+describe('<Banner />', () => {
+  it('should render correctly', () => {
+    const { container } = renderWithTheme(<Banner {...props} />)
+
+    expect(
+      screen.getByRole('heading', { name: /defy death/i })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: /play the new crashlands season/i })
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('img', { name: /defy death/i })).toBeInTheDocument()
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+})
