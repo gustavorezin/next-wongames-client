1efae0eed291fd8e6adc3c68d35bfbdbf50304a6
Gustavo Rezin Durigon <gustavo.rezin@gmail.com>
2024-08-22 10:08:50 -0300

    :sparkles: feat: "casca" do novo componente `Slider`

---
+++ src/components/slider/index.tsx
@@ -0,0 +1,17 @@
+import SlickSlider, { Settings } from 'react-slick'
+import * as S from './styles'
+
+export type SliderSettings = Settings
+
+export type SliderProps = {
+  children: React.ReactNode
+  settings: SliderSettings
+}
+
+export function Slider({ children, settings }: SliderProps) {
+  return (
+    <S.Wrapper>
+      <SlickSlider {...settings}>{children}</SlickSlider>
+    </S.Wrapper>
+  )
+}
